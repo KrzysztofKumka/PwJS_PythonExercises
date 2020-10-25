bbf620c9ef9f75d6a595d73f63ceefd63e72f8ca
kumka <kkumka@student.agh.edu.pl>
2020-10-25 16:46:19 +0100

    wprowadzanie_danych
    KK

--- wprowadzanie_danych.py
+++ wprowadzanie_danych.py
@@ -2,4 +2,4 @@ print('Enter your name, surname and year of birth: ')
 
 x, y, z = map(str, input().split())
 
-print('Name: ' + x + '\nSurname: ' + y + '\nYear of birth: ')
+print('Name: ' + x + '\nSurname: ' + y + '\nYear of birth: ' + z)
