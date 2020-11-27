cb3db4be64ffef62c04995aea2d4dd50080657a7
kumka <kkumka@student.agh.edu.pl>
2020-11-27 14:53:11 +0100

    Iloczyn skalarny

--- iloczyn_skalarny.py
+++ iloczyn_skalarny.py
@@ -1,1 +1,29 @@
-def scalar_product(a, b)
+def scalar_product(a, b):
+    n = len(a)
+    m = len(b)
+    c = []
+
+    for i in range (0, n):
+        x = 0
+        x = a[i]*b[i]
+        c.append(x)
+
+    result = sum(c)
+
+    return result
+
+def main():
+    a = [1, 2, 12, 4]
+    b = [2, 4, 2, 8]
+    y = 0
+
+    print(a)
+    print(b)
+
+    if len(a) == len(b):
+        y = scalar_product(a, b)
+
+    print(y)
+
+if __name__ == '__main__':
+    main()
