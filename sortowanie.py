c86227c95bce640361064b9ec97729cf049f035c
kumka <kkumka@student.agh.edu.pl>
2020-11-27 14:41:46 +0100

    Sortowanie

--- sortowanie.py
+++ sortowanie.py
@@ -2,18 +2,18 @@ import numpy as np
 
 def sort_fun(t):
     n = len(t)
-    for i in range(0, n-1):
-        for j in range (1, n-1):
-            if t[j-1] > t[j]:
+    for i in range(0, n - 1):
+        for j in range(1, n - 1):
+            if t[j - 1] > t[j]:
                 x = t[j]
-                t[j] = t[j-1]
-                t[j-1] = x
-
+                t[j] = t[j - 1]
+                t[j - 1] = x
     return t
+
 
 def main():
     t = []
-    for i in range (0, 50):
+    for i in range(0, 50):
         t.append(np.random.randint(0, 100))
 
     print(t)
@@ -21,6 +21,7 @@ def main():
     sorted_t = sort_fun(t)
 
     print(sorted_t)
+
 
 if __name__ == '__main__':
     main()
